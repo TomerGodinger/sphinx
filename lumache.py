1eef0075fd5729296b4993409730f66d7cc67ba0
Tomer Godinger <tomer.godinger@redis.com>
2022-03-30 16:02:36 +0300

    Another change test

--- lumache.py
+++ lumache.py
@@ -1,6 +1,6 @@
 def get_random_ingredients(kind=None):
     """
-    Return a list of random ingredients as strings!
+    Return a list of random ingredients as strings.
 
     :param kind: Optional "kind" of ingredients.
     :type kind: list[str] or None
