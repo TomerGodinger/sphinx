3861c247df948926d14e3eef21b6ddefa2bfe4fa
Tomer Godinger <tomer.godinger@redis.com>
2022-04-03 14:14:03 +0300

    Changed requirements.txt to hopefully make GitHub Action work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    # 'sphinx_markdown_builder',
+    'sphinx_markdown_builder',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
