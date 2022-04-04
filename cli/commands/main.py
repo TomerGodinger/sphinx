59e4c64c6b7adc4a7df9fd848a121c5989a0b84c
Tomer Godinger <tomer.godinger@redis.com>
2022-04-04 11:40:39 +0300

    Add informative "demo" page and differentiating content for main branch.

--- cli/commands/main.py
+++ cli/commands/main.py
@@ -29,4 +29,17 @@ def order_pastas(dish: str, quantity: int):
     The :option:`order-pastas -dish <main-order-pastas -dish>` argument for this command is similar to the
     :option:`meat -dish <main-order-meat -dish>` argument in the one above.
     """
-    click.echo('Pastas are a mustas! {quantity} servings of {dish} please!')
+    click.echo(f'Pastas are a mustas! {quantity} servings of {dish} please!')
+
+@main_group.command()
+@click.option('-topping', metavar='<name>', envvar='TOPPING', default=None,
+              show_default=True, help='What to put on the pizza')
+def order_pizza(topping: str):
+    """
+    Orders tray of hot pizza.
+    
+    You can choose which topping to use with the :option:`-topping <main-order-pizza -topping>` option.
+    We recommend the olives in particular; they go great on a pizza.
+    """
+    
+    click.echo(f'One pizza please! And put some {topping[::-1]} on it please!')
