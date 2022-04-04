9f007342b52b215e4f75271c8ec0ed633aaa3054
Tomer Godinger <tomer.godinger@redis.com>
2022-04-04 11:45:13 +0300

    Add publish script to main branch for convenience

---
+++ publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+make clean
+make ghpages
+
+git switch gh-pages
+
+for dirname in html/*
+do
+    vername=${dirname##*/}
+    mv "$dirname" "$vername"
+    git add "$vername"
+done
+rmdir html/
+
+git commit -m "publish.sh: Add HTML docs"
+git push origin gh-pages
+
+git switch $CURRENT_BRANCH
